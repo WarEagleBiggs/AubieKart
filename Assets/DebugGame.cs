37639331794fd069f69ab8da6a719137ac7581f7
Adam Biggs <wareaglebiggs@gmail.com>
2025-02-21 11:13:58 -0600

    Added map toggle

--- Assets/DebugGame.cs
+++ Assets/DebugGame.cs
@@ -10,6 +10,9 @@ public class DebugGame : MonoBehaviour
     public GameObject KART;
     public GameObject REF;
 
+    public GameObject map1;
+    public GameObject map2;
+
     private void Update()
     {
         REF.transform.position = new Vector3(KART.transform.position.x, REF.transform.position.y,
@@ -25,5 +28,21 @@ public class DebugGame : MonoBehaviour
         KART.transform.position = REF.transform.position;
         KART.transform.rotation = REF.transform.rotation;
 
+    }
+
+    public void ToggleMap()
+    {
+        if (!map1.activeSelf)
+        {
+            Stuck();
+            map1.SetActive(true);
+            map2.SetActive(false);
+        }
+        else
+        {
+            Stuck();
+            map1.SetActive(false);
+            map2.SetActive(true);
+        }
     }
 }
