3f4980f1b604de923c4422fae9ff3d88875287c5
Adam Biggs <wareaglebiggs@gmail.com>
2025-09-20 19:11:47 -0500

    Added tread marks

--- Assets/QuickOutline/Scripts/Outline.cs
+++ Assets/QuickOutline/Scripts/Outline.cs
@@ -2,8 +2,7 @@
 //  Outline.cs
 //  QuickOutline
 //
-//  Created by Chris Nolet on 3/30/18.
-//  Copyright © 2018 Chris Nolet. All rights reserved.
+//  Modified to ignore objects on the OUTLINE layer
 //
 
 using System;
@@ -12,7 +11,6 @@ using System.Linq;
 using UnityEngine;
 
 [DisallowMultipleComponent]
-
 public class Outline : MonoBehaviour {
   private static HashSet<Mesh> registeredMeshes = new HashSet<Mesh>();
 
@@ -63,7 +61,6 @@ public class Outline : MonoBehaviour {
   private float outlineWidth = 2f;
 
   [Header("Optional")]
-
   [SerializeField, Tooltip("Precompute enabled: Per-vertex calculations are performed in the editor and serialized with the object. "
   + "Precompute disabled: Per-vertex calculations are performed at runtime in Awake(). This may cause a pause for large meshes.")]
   private bool precomputeOutline;
@@ -79,8 +76,10 @@ public class Outline : MonoBehaviour {
   private Material outlineFillMaterial;
 
   private bool needsUpdate;
+  private int outlineLayer;
 
   void Awake() {
+    outlineLayer = LayerMask.NameToLayer("OUTLINE");
 
     // Cache renderers
     renderers = GetComponentsInChildren<Renderer>();
@@ -101,29 +100,23 @@ public class Outline : MonoBehaviour {
 
   void OnEnable() {
     foreach (var renderer in renderers) {
+      if (renderer.gameObject.layer == outlineLayer) continue;
 
-      // Append outline shaders
       var materials = renderer.sharedMaterials.ToList();
-
       materials.Add(outlineMaskMaterial);
       materials.Add(outlineFillMaterial);
-
       renderer.materials = materials.ToArray();
     }
   }
 
   void OnValidate() {
-
-    // Update material properties
     needsUpdate = true;
 
-    // Clear cache when baking is disabled or corrupted
     if (!precomputeOutline && bakeKeys.Count != 0 || bakeKeys.Count != bakeValues.Count) {
       bakeKeys.Clear();
       bakeValues.Clear();
     }
 
-    // Generate smooth normals when baking is enabled
     if (precomputeOutline && bakeKeys.Count == 0) {
       Bake();
     }
@@ -132,44 +125,34 @@ public class Outline : MonoBehaviour {
   void Update() {
     if (needsUpdate) {
       needsUpdate = false;
-
       UpdateMaterialProperties();
     }
   }
 
   void OnDisable() {
     foreach (var renderer in renderers) {
+      if (renderer.gameObject.layer == outlineLayer) continue;
 
-      // Remove outline shaders
       var materials = renderer.sharedMaterials.ToList();
-
       materials.Remove(outlineMaskMaterial);
       materials.Remove(outlineFillMaterial);
-
       renderer.materials = materials.ToArray();
     }
   }
 
   void OnDestroy() {
-
-    // Destroy material instances
     Destroy(outlineMaskMaterial);
     Destroy(outlineFillMaterial);
   }
 
   void Bake() {
-
-    // Generate smooth normals for each mesh
     var bakedMeshes = new HashSet<Mesh>();
 
     foreach (var meshFilter in GetComponentsInChildren<MeshFilter>()) {
+      if (meshFilter.gameObject.layer == outlineLayer) continue;
 
-      // Skip duplicates
-      if (!bakedMeshes.Add(meshFilter.sharedMesh)) {
-        continue;
-      }
+      if (!bakedMeshes.Add(meshFilter.sharedMesh)) continue;
 
-      // Serialize smooth normals
       var smoothNormals = SmoothNormals(meshFilter.sharedMesh);
 
       bakeKeys.Add(meshFilter.sharedMesh);
@@ -178,72 +161,45 @@ public class Outline : MonoBehaviour {
   }
 
   void LoadSmoothNormals() {
-
-    // Retrieve or generate smooth normals
     foreach (var meshFilter in GetComponentsInChildren<MeshFilter>()) {
+      if (meshFilter.gameObject.layer == outlineLayer) continue;
 
-      // Skip if smooth normals have already been adopted
-      if (!registeredMeshes.Add(meshFilter.sharedMesh)) {
-        continue;
-      }
+      if (!registeredMeshes.Add(meshFilter.sharedMesh)) continue;
 
-      // Retrieve or generate smooth normals
       var index = bakeKeys.IndexOf(meshFilter.sharedMesh);
       var smoothNormals = (index >= 0) ? bakeValues[index].data : SmoothNormals(meshFilter.sharedMesh);
 
-      // Store smooth normals in UV3
       meshFilter.sharedMesh.SetUVs(3, smoothNormals);
 
-      // Combine submeshes
       var renderer = meshFilter.GetComponent<Renderer>();
-
       if (renderer != null) {
         CombineSubmeshes(meshFilter.sharedMesh, renderer.sharedMaterials);
       }
     }
 
-    // Clear UV3 on skinned mesh renderers
     foreach (var skinnedMeshRenderer in GetComponentsInChildren<SkinnedMeshRenderer>()) {
+      if (skinnedMeshRenderer.gameObject.layer == outlineLayer) continue;
 
-      // Skip if UV3 has already been reset
-      if (!registeredMeshes.Add(skinnedMeshRenderer.sharedMesh)) {
-        continue;
-      }
+      if (!registeredMeshes.Add(skinnedMeshRenderer.sharedMesh)) continue;
 
-      // Clear UV3
       skinnedMeshRenderer.sharedMesh.uv4 = new Vector2[skinnedMeshRenderer.sharedMesh.vertexCount];
-
-      // Combine submeshes
       CombineSubmeshes(skinnedMeshRenderer.sharedMesh, skinnedMeshRenderer.sharedMaterials);
     }
   }
 
   List<Vector3> SmoothNormals(Mesh mesh) {
-
-    // Group vertices by location
     var groups = mesh.vertices.Select((vertex, index) => new KeyValuePair<Vector3, int>(vertex, index)).GroupBy(pair => pair.Key);
-
-    // Copy normals to a new list
     var smoothNormals = new List<Vector3>(mesh.normals);
 
-    // Average normals for grouped vertices
     foreach (var group in groups) {
+      if (group.Count() == 1) continue;
 
-      // Skip single vertices
-      if (group.Count() == 1) {
-        continue;
-      }
-
-      // Calculate the average normal
       var smoothNormal = Vector3.zero;
-
       foreach (var pair in group) {
         smoothNormal += smoothNormals[pair.Value];
       }
-
       smoothNormal.Normalize();
 
-      // Assign smooth normal to each vertex
       foreach (var pair in group) {
         smoothNormals[pair.Value] = smoothNormal;
       }
@@ -253,25 +209,14 @@ public class Outline : MonoBehaviour {
   }
 
   void CombineSubmeshes(Mesh mesh, Material[] materials) {
+    if (mesh.subMeshCount == 1) return;
+    if (mesh.subMeshCount > materials.Length) return;
 
-    // Skip meshes with a single submesh
-    if (mesh.subMeshCount == 1) {
-      return;
-    }
-
-    // Skip if submesh count exceeds material count
-    if (mesh.subMeshCount > materials.Length) {
-      return;
-    }
-
-    // Append combined submesh
     mesh.subMeshCount++;
     mesh.SetTriangles(mesh.triangles, mesh.subMeshCount - 1);
   }
 
   void UpdateMaterialProperties() {
-
-    // Apply properties according to mode
     outlineFillMaterial.SetColor("_OutlineColor", outlineColor);
 
     switch (outlineMode) {
